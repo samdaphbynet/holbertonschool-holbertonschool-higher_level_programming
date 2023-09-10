9174ca455979ba289ee55992f7e8e187c726f101
samdaphbynet <lj8@hotmail.fr>
2023-09-11 01:53:36 +0200

    Script that updates the color the header element when the user clicks on the tag with id 'red_header'

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,6 @@
+let header = document.querySelector("header");
+const red_header = document.getElementById("red_header");
+
+red_header.addEventListener("click", function() {
+    header.style.color = "#FF0000";
+});
