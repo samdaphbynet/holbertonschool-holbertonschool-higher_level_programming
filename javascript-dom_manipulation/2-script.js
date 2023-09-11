57b872e8a1a5677345cd952ef53712ca67632942
samdaphbynet <lj8@hotmail.fr>
2023-09-11 14:17:26 +0200

    Script that adds the class to element when the user clicks on the tag

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,6 @@
+let header = document.querySelector("header");
+const red_header = document.getElementById("red_header");
+
+red_header.addEventListener("click", () => {
+    header.classList.add("red");
+})
